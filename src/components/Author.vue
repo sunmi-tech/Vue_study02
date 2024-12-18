bc906e2fbd181c4f859ca75a23f645e8dcde6a0f
sunmi An <aseonmi220@gmail.com>
2024-12-18 13:04:38 +0900

    first commit

---
+++ src/components/Author.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>작가소개입니다.</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h5 @click="$router.push('/detail/0')">{{blog[0].title}}</h5>
+        <p>{{blog[0].date}}</p>
+    </div>
+    <div>
+        <h5>{{blog[1].title}}</h5>
+        <p>{{blog[1].date}}</p>
+    </div>
+    <div>
+        <h5>{{blog[2].title}}</h5>
+        <p>{{blog[2].date}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'List',
+    props : {
+        blog : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
